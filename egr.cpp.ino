83e7e93730f66e8056ba1a6194ce4a43cc57c239
jonas <94145174+arael34@users.noreply.github.com>
2023-01-17 00:05:42 -0500

    Create egr.cpp.ino

---
+++ egr.cpp.ino
@@ -0,0 +1,42 @@
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_FreeTouch.h>
+
+#define TOUCH_PIN 0 // I assume 0 is fine
+#define PIXEL_PIN 1
+#define NUM_LEDS 16 // find out actual pixel count
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, PIXEL_PIN);
+// read docs for args
+Adafruit_FreeTouch touchSensor = Adafruit_FreeTouch(
+  CAPTOUCH_PIN, OVERSAMPLE_4, RESISTOR_50K, FREQ_MODE_NONE
+);
+
+uint32_t prevTime;          // Time of last animation mode switch
+
+// For calibration: change this variable to something between
+// your capacitive touch serial readouts for on and off
+// without proper output this will maybe have to be guessed
+int touch = 650;
+
+uint8_t state = 0;
+
+// Modes --------------------------------------------------------------
+void gradient() {
+  
+}
+// --------------------------------------------------------------------
+
+// ran once at the start
+void setup() {
+  prevTime = millis();
+  pixels.begin();
+  pixels.setBrightness(60); // ~1/3 brightness
+  if (!touchSensor.begin()) {
+    // error handling ig
+  }
+}
+
+// loop code
+void loop() {
+  // touch logic 
+}
